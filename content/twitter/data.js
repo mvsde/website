00d3b6a64a68f1b0ff5192261ce23fb0396621a1
Fynn Becker <fynn.becker@lfda.de>
2020-01-08 10:36:48 +0100

    fix: Remove date-based Twitter stuff

--- content/twitter/data.js
+++ content/twitter/data.js
@@ -29,42 +29,4 @@ async function getTweets () {
 
 const tweets = getTweets()
 
-async function getDates (type) {
-  return (await tweets).reduce((accumulator, current) => {
-    const dateString = current.date.toISOString()
-
-    let date
-
-    if (type === 'years') {
-      date = dateString.slice(0, 4).split('-')
-    }
-
-    if (type === 'months') {
-      date = dateString.slice(0, 7).split('-')
-    }
-
-    if (type === 'days') {
-      date = dateString.slice(0, 10).split('-')
-    }
-
-    const previous = accumulator[accumulator.length - 1]
-
-    const data = {
-      name: date.join('-'),
-      path: date.join('/'),
-      children: []
-    }
-
-    if (!previous || previous.path !== data.path) {
-      accumulator.push(data)
-    }
-
-    return accumulator
-  }, [])
-}
-
 exports.tweets = tweets
-
-exports.getYears = () => getDates('years')
-exports.getMonths = () => getDates('months')
-exports.getDays = () => getDates('days')
--- twitter.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-steps=( "date" "index" "list" "months" "tweet" "years" )
-
-for item in "${steps[@]}"
-do
-  echo "Building Twitter/$item…"
-  node_modules/.bin/eleventy --input content/twitter/$item.11ty.js --output dist/twitter --quiet
-done
