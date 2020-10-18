44ae9ba68af5d88920762476f28104609b27b4fa
Fynn Becker <fynn@hey.com>
2020-10-18 09:40:42 +0200

    refactor: De-nest Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,26 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({
-    'content/img': 'img',
-    'src/fonts': '/fonts',
-    'src/icon': '/'
-  })
+const COPY_FILES = {
+  'content/img': 'img',
+  'src/fonts': '/fonts',
+  'src/icon': '/'
+}
 
-  eleventyConfig.addPairedShortcode("lang", function (content, lang) {
-    return `<span lang="${lang}">${content}</span>`
-  })
+/**
+ * Create HTML with specific language code
+ * @param {string} content Content between tags
+ * @param {string} lang Language code
+ * @returns {string} Content wrapped with language
+ */
+function langShortcode (content, lang) {
+  return `<span lang="${lang}">${content}</span>`
+}
+
+/**
+ * Eleventy configuration
+ * @param {Object} eleventyConfig Eleventy configuration
+ */
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy(COPY_FILES)
+  eleventyConfig.addPairedShortcode("lang", langShortcode)
 
   return {
     dir: {
