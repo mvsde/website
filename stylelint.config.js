e29f3cfbd0f2dc14d29cc05ae78f91d456d3de4c
Fynn Becker <post@fynn.be>
2023-06-02 22:17:34 +0200

    chore(css): Sort properties alphabetically

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,11 @@
 module.exports = {
 	extends: ['stylelint-config-standard'],
+	plugins: ['stylelint-order'],
 	rules: {
 		'selector-class-pattern': null,
 		'custom-property-pattern': null,
 		'custom-property-empty-line-before': null,
+		'order/properties-alphabetical-order': true,
 	},
 	overrides: [
 		{
